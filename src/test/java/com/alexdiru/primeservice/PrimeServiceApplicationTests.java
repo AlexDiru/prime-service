ccf3800fea7ecb03b7ff6c97a3d821a666309942
Alex Spedding <alex@Alexs-MacBook-Pro-16.local>
2022-12-02 18:26:15 +0100

    Initial commit

---
+++ src/test/java/com/alexdiru/primeservice/PrimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexdiru.primeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
